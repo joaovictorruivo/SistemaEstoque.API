fbcff19d488f231b0cdb9fd7ac5c0a15e38a4bca
joaovictorruivo <joao4920oliveira@gmail.com>
2026-02-24 22:06:17 -0300

    feat: configurar EF Core Migrations e aplicar validações em Produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,11 +1,20 @@
-﻿namespace SistemaEstoque.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaEstoque.API.Models
 {
     public class Produto
     {
+        [Key] // Define explicitamente como chave primária
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public decimal Preco { get; set; }
-        public int Quantidade { get; set; }
 
+        [Required(ErrorMessage = "O nome do produto é obrigatório")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Range(0.01, 999999, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Preco { get; set; }
+
+        [Range(0, 10000, ErrorMessage = "A quantidade não pode ser negativa")]
+        public int Quantidade { get; set; }
     }
 }
