7fd59211673a090c3d80f7325590188a8be3e3f6
João Victor <joao4920oliveira@gmail.com>
2026-03-03 22:43:20 -0300

    feat: cria CategoriasController com endpoints de GET e POST

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaEstoque.API.Data;
+using SistemaEstoque.API.Models;
+
+namespace SistemaEstoque.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categorias
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categorias = await _context.Categorias.ToListAsync();
+            return Ok(categorias);
+        }
+
+        // POST: api/Categorias
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            // Retorna 201 Created
+            return CreatedAtAction(nameof(Get), new { id = categoria.Id }, categoria);
+        }
+    }
+}
